d3284916d7bdc915f454eb15fcf6406b327b8812
Jason Ayre <jasonayre@gmail.com>
2016-05-16 16:03:15 -0600

    load railtie if rails present

--- lib/resource_subscriber.rb
+++ lib/resource_subscriber.rb
@@ -7,24 +7,14 @@ module ResourceSubscriber
   extend ::ActiveSupport::Autoload
 
   autoload :AutoInject
-  autoload :Config
   autoload :Changes
   autoload :Message
   autoload :Middlewares
+  autoload :Railtie
   autoload :Resourceful
   autoload :Publishable
   autoload :Publisher
   autoload :PublisherConfig
 
-  def self.config
-    @_config ||= ::ResourceSubscriber::Config.new
-  end
-
-  def self.publisher_config
-    @_publisher_config ||= ::ResourceSubscriber::PublisherConfig.new
-  end
-
-  def self.configure(&block)
-    block.call(config)
-  end
+  require "resource_subscriber/railtie" if defined?(Rails)
 end
