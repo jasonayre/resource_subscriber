ccc1fdb5351ff6f2b4aecf985717554175a44e72
Jason Ayre <jayre@peer60.com>
2016-05-18 12:55:41 -0600

    message sets ivars/support changes if present or previous changes

--- lib/resource_subscriber/message.rb
+++ lib/resource_subscriber/message.rb
@@ -1,7 +1,23 @@
 module ResourceSubscriber
-  class Message < ::Trax::Core::Types::Struct
-    string :resource_type
-    json :resource
-    json :changes
+  class Message
+    include ActiveModel::Serializers::JSON
+
+    attr_accessor :resource_type, :resource, :changes
+
+    def initialize(resource)
+      @resource_type = resource.class.base_class.name
+      @changes = resource.changes.blank? ? resource.previous_changes : resource.changes
+      @resource = resource.attributes
+    end
+
+    def attributes=(hash)
+      hash.each do |key, value|
+        send("#{key}=", value)
+      end
+    end
+
+    def attributes
+      instance_values
+    end
   end
 end
